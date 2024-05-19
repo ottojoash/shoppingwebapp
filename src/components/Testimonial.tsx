6d0df2beea5c89c2ce647f9787af90e5bcebe119
ottojoash <34677630+ottojoash@users.noreply.github.com>
2024-05-19 10:58:01 +0000

    test

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -32,7 +32,7 @@ const Testimonial = () => {
             </div>
           </div>
 
-          <div className="bg-red-600 bg-[url(/cta-banner.jpg)] bg-cover h-[500px] rounded-2xl grid place-items-center">
+          {/* <div className="bg-red-600 bg-[url(/cta-banner.jpg)] bg-cover h-[500px] rounded-2xl grid place-items-center">
             <div className="bg-[#ffffffab] min-w-[270px] sm:min-w-[300px] md:min-w-[500px] rounded-xl py-8 sm:px-8 px-2 grid place-items-center gap-3">
               <button className="bg-blackish text-white p-2 rounded-md">
                 25% DISCOUNT
@@ -44,7 +44,7 @@ const Testimonial = () => {
                 Starting @ $20 <b>Shop Now</b>
               </p>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
