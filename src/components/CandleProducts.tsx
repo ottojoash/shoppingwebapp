4b8b7881ba2108ecf083fe40c9e70b4f6a29c281
ottojoash <34677630+ottojoash@users.noreply.github.com>
2024-05-19 05:24:21 +0000

    candle

--- src/components/CandleProducts.tsx
+++ src/components/CandleProducts.tsx
@@ -15,6 +15,9 @@ const candleProducts: CandleProduct[] = [
   },
   {
     img: "https://via.placeholder.com/300x300?text=A+Walk+in+the+Woods",
+  },
+  {
+    img: "https://via.placeholder.com/300x300?text=A+Walk+in+the+Woods",
   }
 ];
 
@@ -26,8 +29,8 @@ interface CandleProductCardProps {
 // Single candle product card component
 const CandleProductCard: React.FC<CandleProductCardProps> = ({ img }) => {
   return (
-    <div className="flex-none w-1/3 p-4 bg-white shadow-lg border rounded-lg" style={{ height: '300px' }}>
-      <img src={img} alt="Candle" className="h-full w-full object-cover"/>
+    <div className="flex-none w-[90px] h-[100px] sm:w-[150px] sm:h-[150px] p-2 bg-white shadow-lg border rounded-lg">
+      <img src={img} alt="Candle" className="h-full w-full object-cover rounded-lg"/>
     </div>
   );
 };
@@ -35,8 +38,8 @@ const CandleProductCard: React.FC<CandleProductCardProps> = ({ img }) => {
 // Main component to display all candle products
 const CandleProducts: React.FC = () => {
   return (
-    <div className="flex justify-center w-full overflow-x-auto px-4 sm:px-6 lg:px-8">
-      <div className="flex space-x-4 min-w-max"> {/* Use flex and min-w-max to enable horizontal scrolling */}
+    <div className="flex justify-center w-full overflow-x-auto px-2 sm:px-4 lg:px-8">
+      <div className="flex space-x-2 sm:space-x-4 min-w-max"> {/* Use flex and min-w-max to enable horizontal scrolling */}
         {candleProducts.map((product, index) => (
           <CandleProductCard key={index} img={product.img} />
         ))}
