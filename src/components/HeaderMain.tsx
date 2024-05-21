c1cb0994050bc1c3d4db099b2eefef67bd3fc964
ottojoash <34677630+ottojoash@users.noreply.github.com>
2024-05-21 12:47:21 +0000

    header

--- src/components/HeaderMain.tsx
+++ src/components/HeaderMain.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-
-import { BsSearch } from "react-icons/bs";
 import { BiUser } from "react-icons/bi";
 import { FiHeart } from "react-icons/fi";
 import { HiOutlineShoppingBag } from "react-icons/hi";
@@ -8,40 +6,15 @@ import { HiOutlineShoppingBag } from "react-icons/hi";
 const HeaderMain = () => {
   return (
     <div className="border-b border-gray-200 py-6">
-      <div className="container sm:flex justify-between items-center">
-        <div className="font-bold text-4xl text-center pb-4 sm:pb-0 text-blackish">
-          Theessentialman
+      <div className="container flex flex-col sm:flex-row justify-between items-center text-center sm:text-left">
+        <div className="font-bold text-4xl pb-4 sm:pb-0 text-blackish">
+          The Essential Man
         </div>
 
-        <div className="w-full sm:w-[300px] md:w-[70%] relative">
-          <input
-            className="border-gray-200 border p-2 px-4 rounded-lg w-full"
-            type="text"
-            placeholder="Enter any product name..."
-          />
-
-          <BsSearch
-            className="absolute right-0 top-0 mr-3 mt-3 text-gray-400"
-            size={20}
-          />
-        </div>
-
-        <div className="hidden lg:flex gap-4 text-gray-500 text-[30px]">
-          <BiUser />
-
-          <div className="relative">
-            <FiHeart />
-            <div className="bg-red-600 rounded-full absolute top-0 right-0 w-[18px] h-[18px] text-[12px] text-white grid place-items-center translate-x-1 -translate-y-1">
-              0
-            </div>
-          </div>
-
-          <div className="relative">
-            <HiOutlineShoppingBag />
-            <div className="bg-red-600 rounded-full absolute top-0 right-0 w-[18px] h-[18px] text-[12px] text-white grid place-items-center translate-x-1 -translate-y-1">
-              0
-            </div>
-          </div>
+        <div className="flex justify-center gap-8 text-gray-600 text-lg mb-4 sm:mb-0">
+          <a href="/shop" className="hover:text-black">Shop</a>
+          <a href="/review" className="hover:text-black">Review</a>
+          <a href="/about" className="hover:text-black">About</a>
         </div>
       </div>
     </div>
