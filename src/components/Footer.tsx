2754e58c954709846d59b07d74af240bec7780fb
ottojoash <34677630+ottojoash@users.noreply.github.com>
2024-05-21 12:55:16 +0000

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,22 +3,22 @@ import { FaInstagram, FaFacebookF, FaTwitter, FaWhatsapp, FaEnvelope } from "rea
 
 const Footer = () => {
   return (
-    <div className="bg-blackish text-gray-500 text-center py-4 pb-16 md:pb-4">
+    <div className="bg-black text-gray-500 text-center py-4 pb-16 md:pb-4">
       <div className="flex justify-center space-x-4 mb-4">
-        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-white">
-          <FaInstagram size={32} />
+        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="bg-gray-700 hover:bg-white p-2 rounded-full transition-colors duration-300">
+          <FaInstagram size={24} className="text-white hover:text-black"/>
         </a>
-        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-white">
-          <FaFacebookF size={32} />
+        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="bg-gray-700 hover:bg-white p-2 rounded-full transition-colors duration-300">
+          <FaFacebookF size={24} className="text-white hover:text-black"/>
         </a>
-        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-white">
-          <FaTwitter size={32} />
+        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" className="bg-gray-700 hover:bg-white p-2 rounded-full transition-colors duration-300">
+          <FaTwitter size={24} className="text-white hover:text-black"/>
         </a>
-        <a href="https://www.whatsapp.com" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-white">
-          <FaWhatsapp size={32} />
+        <a href="https://www.whatsapp.com" target="_blank" rel="noopener noreferrer" className="bg-gray-700 hover:bg-white p-2 rounded-full transition-colors duration-300">
+          <FaWhatsapp size={24} className="text-white hover:text-black"/>
         </a>
-        <a href="mailto:info@example.com" className="text-gray-500 hover:text-white">
-          <FaEnvelope size={32} />
+        <a href="mailto:info@example.com" className="bg-gray-700 hover:bg-white p-2 rounded-full transition-colors duration-300">
+          <FaEnvelope size={24} className="text-white hover:text-black"/>
         </a>
       </div>
       <div>
@@ -29,4 +29,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
