0be59f58ec504ba2faf6f83474af5f7d45a7a5a0
ottojoash <louisjoshbricks@gmail.com>
2024-06-24 07:32:20 +0300

    link

--- src/components/HeaderMain.tsx
+++ src/components/HeaderMain.tsx
@@ -1,22 +1,23 @@
-import React from "react";
-import Image from 'next/image';
+import React from 'react';
+import Link from 'next/link';
 
-
-const HeaderMain = () => {
+const HeaderMain: React.FC = () => {
   return (
     <div className="border-b border-gray-200 py-6">
-      <div className="container flex flex-col sm:flex-row justify-between items-center text-center sm:text-left">
-        
-        {/* Image replacing text */}
+      <div className="container mx-auto flex flex-col sm:flex-row justify-between items-center text-center sm:text-left">
         <div className="pb-4 sm:pb-0">
-        {/* <Image src="./assets/logo.jpg" alt="The Essential Man" width={500} height={300} /> */}
-          <img src="./assets/logo.jpg" alt="The Essential Man" className="h-10" /> 
+          <img src="/assets/logo.jpg" alt="The Essential Man" className="h-10" />
         </div>
-
         <div className="flex justify-center gap-8 text-gray-600 text-lg mb-4 sm:mb-0">
-          <a href="/shop" className="hover:text-black">Shop</a>
-          <a href="/review" className="hover:text-black">Review</a>
-          <a href="/about" className="hover:text-black">About</a>
+          <Link href="/shop" legacyBehavior>
+            <a className="hover:text-black">Shop</a>
+          </Link>
+          <Link href="/review" legacyBehavior>
+            <a className="hover:text-black">Review</a>
+          </Link>
+          <Link href="/about" legacyBehavior>
+            <a className="hover:text-black">About</a>
+          </Link>
         </div>
       </div>
     </div>
